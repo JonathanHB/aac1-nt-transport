992f56da596451a9188144aad0e2ea25ff1b0b24
Jonathan Borowsky <jonathan3141@gmail.com>
2026-01-14 12:56:11 -0800

    pc updates

--- metadynamics/run3/scripts/copy_mtd_states.sh
+++ metadynamics/run3/scripts/copy_mtd_states.sh
@@ -8,7 +8,7 @@ read -s PASSWORD
 #    printf '%02d' "$1"
 #}
 
-runnum=2
+runnum=3
 segnum=$1
 
 cd run_c
@@ -17,7 +17,7 @@ sshpass -p "$PASSWORD" scp jborowsky@dt1.wynton.ucsf.edu:/wynton/home/grabe/jbor
 
 for i in {0..7};
 do
-	sshpass -p "$PASSWORD" scp jborowsky@dt1.wynton.ucsf.edu:/wynton/home/grabe/jborowsky/aac1-nt-transport/metadynamics/run$runnum/run_c/$i/mtd_seg_$(printf '%02d' "$segnum").gro   c_run$(printf '%02d' "$i")_mtd_seg_$(printf '%02d' "$segnum").gro
+	sshpass -p "$PASSWORD" scp jborowsky@dt1.wynton.ucsf.edu:/wynton/home/grabe/jborowsky/aac1-nt-transport/metadynamics/run$runnum/run_c/$i/mtd_seg_$(printf '%02d' "$segnum").part*.gro   c_run$(printf '%02d' "$i")_mtd_seg_$(printf '%02d' "$segnum").gro
 	echo 1 0 | gmx trjconv -f c_run$(printf '%02d' "$i")_mtd_seg_$(printf '%02d' "$segnum").gro -s mdp_hmr_4fs_01.tpr -pbc mol -center -o c_run$(printf '%02d' "$i")_mtd_seg_$(printf '%02d' "$segnum")_centered.gro
 done
 
@@ -27,7 +27,7 @@ sshpass -p "$PASSWORD" scp jborowsky@dt1.wynton.ucsf.edu:/wynton/home/grabe/jbor
 
 for i in {0..7};
 do
-	sshpass -p "$PASSWORD" scp jborowsky@dt1.wynton.ucsf.edu:/wynton/home/grabe/jborowsky/aac1-nt-transport/metadynamics/run$runnum/run_m/$i/mtd_seg_$(printf '%02d' "$segnum").gro   m_run$(printf '%02d' "$i")_mtd_seg_$(printf '%02d' "$segnum").gro
+	sshpass -p "$PASSWORD" scp jborowsky@dt1.wynton.ucsf.edu:/wynton/home/grabe/jborowsky/aac1-nt-transport/metadynamics/run$runnum/run_m/$i/mtd_seg_$(printf '%02d' "$segnum").part*.gro   m_run$(printf '%02d' "$i")_mtd_seg_$(printf '%02d' "$segnum").gro
 	echo 1 0 | gmx trjconv -f m_run$(printf '%02d' "$i")_mtd_seg_$(printf '%02d' "$segnum").gro -s mdp_hmr_4fs_01.tpr -pbc mol -center -o m_run$(printf '%02d' "$i")_mtd_seg_$(printf '%02d' "$segnum")_centered.gro
 done
 cd ..
