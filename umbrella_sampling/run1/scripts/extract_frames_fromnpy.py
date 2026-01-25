979b51d71c5a1c09c1dc5e1afdf165c8748ac1f7
Jonathan Borowsky <jonathan3141@gmail.com>
2026-01-25 15:36:42 -0800

    updated start state extraction scripts and plumed.dat

---
+++ umbrella_sampling/run1/scripts/extract_frames_fromnpy.py
@@ -0,0 +1,14 @@
+import numpy as np
+import os
+
+gmx="/wynton/home/grabe/jborowsky/gromacs/gmx_2024.4_plumed_mpi/gromacs/gromacs-2024.3/build/bin/gmx_mpi"
+
+input = "/wynton/home/grabe/jborowsky/aac1-nt-transport/metadynamics/run4/run_c/1"
+
+times = np.load("times_spaced.npy")
+x = 0
+for t in times:
+    print(f"{t} ps")
+    os.system(f"echo 0 | {gmx} trjconv -f {input}/mtd_seg_01.all.xtc -s {input}/mdp_hmr_4fs_01.tpr -o frame_{x}.gro -dump {int(t)}")
+
+    x += 1
--- umbrella_sampling/run1/scripts/start_umbrellas.py
+++ umbrella_sampling/run1/scripts/start_umbrellas.py
@@ -7,5 +7,4 @@ import os
 #nwalkers=32
 #start_states = [0,1,3,4,5,6,7,8,9,19,20,21,22,23,24,26,27,28,29,30,31]
 
-
 os.chdir("run_c")
