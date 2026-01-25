80d2bc4089c94d2e18d9317b093696dc6026df5b
Jonathan Borowsky <jonathan3141@gmail.com>
2026-01-25 13:00:29 -0800

    updated processing notebook

--- relaxation/run1/scripts/copy_mtd_colvar.sh
+++ relaxation/run1/scripts/copy_mtd_colvar.sh
@@ -8,26 +8,34 @@ read -s PASSWORD
 #    printf '%02d' "$1"
 #}
 
-#segnum=$1
-
 echo "copying from run $1"
+
+nwalkers=32
 
 cd run_c
 
-for i in {0..7};
+for ((i=0; i<$nwalkers; i++))
 do
-	sshpass -p "$PASSWORD" scp jborowsky@dt1.wynton.ucsf.edu:/wynton/home/grabe/jborowsky/aac1-nt-transport/metadynamics/run$1/run_c/$i/COLVAR COLVAR.$i
+	if [ ! -d "$i" ]; then
+		mkdir $i
+	fi
+
+	# for ((j=0; j<10; j++))
+	# do
+	sshpass -p "$PASSWORD" scp jborowsky@dt1.wynton.ucsf.edu:/wynton/home/grabe/jborowsky/aac1-nt-transport/relaxation/run$1/run_c/$i/colvars/COLVAR.* $i
+	# done
+
 done
 
-sshpass -p "$PASSWORD" scp jborowsky@dt1.wynton.ucsf.edu:/wynton/home/grabe/jborowsky/aac1-nt-transport/metadynamics/run$1/run_c/summed_hills/fes-$2.dat summed_hills/fes-$2.dat
+# sshpass -p "$PASSWORD" scp jborowsky@dt1.wynton.ucsf.edu:/wynton/home/grabe/jborowsky/aac1-nt-transport/metadynamics/run$1/run_c/summed_hills/fes-$2.dat summed_hills/fes-$2.dat
 
-cd ../run_m
+# cd ../run_m
 
-for i in {0..7};
-do
-	sshpass -p "$PASSWORD" scp jborowsky@dt1.wynton.ucsf.edu:/wynton/home/grabe/jborowsky/aac1-nt-transport/metadynamics/run$1/run_m/$i/COLVAR COLVAR.$i
-done
+# for i in {0..7};
+# do
+# 	sshpass -p "$PASSWORD" scp jborowsky@dt1.wynton.ucsf.edu:/wynton/home/grabe/jborowsky/aac1-nt-transport/metadynamics/run$1/run_m/$i/COLVAR COLVAR.$i
+# done
 
-sshpass -p "$PASSWORD" scp jborowsky@dt1.wynton.ucsf.edu:/wynton/home/grabe/jborowsky/aac1-nt-transport/metadynamics/run$1/run_m/summed_hills/fes-$2.dat summed_hills/fes-$2.dat
+# sshpass -p "$PASSWORD" scp jborowsky@dt1.wynton.ucsf.edu:/wynton/home/grabe/jborowsky/aac1-nt-transport/metadynamics/run$1/run_m/summed_hills/fes-$2.dat summed_hills/fes-$2.dat
 
 cd ..
